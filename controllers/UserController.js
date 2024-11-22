36afaa905220deb24f8776ed6d66475def4715ff
Erick John Orqueza Lopez <112525487+CyberWhisker@users.noreply.github.com>
2024-11-22 12:43:44 +0800

    Update for reset User password

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,41 +1,42 @@
 const Model = require('../models/UserModel')
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const createToken = (_id) => {
-    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' })
 }
 
 const getUsers = async (req, res) => {
     try {
-        const data = await Model.find({}).sort({createdAt: -1})
+        const data = await Model.find({}).sort({ createdAt: -1 })
         res.status(200).json(data)
     } catch (error) {
-        res.status(400).json({error: error.message})
+        res.status(400).json({ error: error.message })
     }
-} 
+}
 
 const getUserById = async (req, res) => {
-    const {id} = req.params
-    if(!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({error: 'Not valid ID'})
+    const { id } = req.params
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'Not valid ID' })
     }
     try {
-        const data = await Model.findOne({_id: id})
+        const data = await Model.findOne({ _id: id })
         res.status(200).json(data)
     } catch (error) {
-        res.status(400).json({error: error.message})
+        res.status(400).json({ error: error.message })
     }
-} 
+}
 
 const login = async (req, res) => {
-    const {email, password} = req.body
+    const { email, password } = req.body
     try {
         const data = await Model.loginHash(email, password)
         const token = createToken(data._id)
-        res.status(200).json({token: token, user: { _id: data._id, email: data.email, role: data.role}})
+        res.status(200).json({ token: token, user: { _id: data._id, email: data.email, role: data.role } })
     } catch (error) {
-        res.status(400).json({error: error.message})
+        res.status(400).json({ error: error.message })
     }
 }
 
@@ -43,77 +44,90 @@ const register = async (req, res) => {
     try {
         const user = await Model.registerHash(req.body)
         const token = createToken(user._id)
-        res.status(200).json({token, user: {_id: user._id, email: user.email, role: user.role}})
+        res.status(200).json({ token, user: { _id: user._id, email: user.email, role: user.role } })
     } catch (error) {
-        res.status(400).json({error: error.message})
+        res.status(400).json({ error: error.message })
     }
 }
 
 const verifyUser = async (req, res) => {
-    const {token} = req.body
+    const { token } = req.body
     try {
         const result = jwt.verify(token, process.env.SECRET)
         if (!result) {
-            res.status(400).json({error: 'invalid token'})
+            res.status(400).json({ error: 'invalid token' })
         }
         const userData = jwt.decode(token)
-        res.status(200).json({user: userData})
+        res.status(200).json({ user: userData })
     } catch (error) {
-        res.status(400).json({error: error.message})
+        res.status(400).json({ error: error.message })
     }
 }
 
 //Delete Data
 const deleteData = async (req, res) => {
-    const {id} = req.params
+    const { id } = req.params
 
-    if(!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({error: 'Not valid ID'})
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'Not valid ID' })
     }
 
-    const data = await Model.findOneAndDelete({_id: id})
+    const data = await Model.findOneAndDelete({ _id: id })
 
     if (!data) {
-        return res.status(404).json({error: 'No record found'})
+        return res.status(404).json({ error: 'No record found' })
     }
 
-    res.status(200).json({message: 'Successfully Deleted'})
+    res.status(200).json({ message: 'Successfully Deleted' })
 }
 
 //Update Data
 const updateData = async (req, res) => {
-    const {id} = req.params
-    if(!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({error: 'Not valid ID'})
+    const { id } = req.params
+    const { newPassword } = req.body
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'Not valid ID' })
     }
 
-    const data = await Model.findOneAndUpdate({_id: id}, {
-        ...req.body,
-        image: req.file ? req.file.filename : null
-    })
-
-    if (!data) {
-        return res.status(404).json({error: 'No record found'})
+    if (newPassword) {
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(newPassword, salt)
+        const data = await Model.findOneAndUpdate({ _id: id }, {
+            ...req.body,
+            image: req.file ? req.file.filename : null,
+            password: hash
+        })
+        if (!data) {
+            return res.status(404).json({ error: 'No record found' })
+        }
+    } else {
+        const data = await Model.findOneAndUpdate({ _id: id }, {
+            ...req.body,
+            image: req.file ? req.file.filename : null,
+        })
+        if (!data) {
+            return res.status(404).json({ error: 'No record found' })
+        }
     }
 
     res.status(200).json(req.body)
 }
 
 const getUsersWithAttendanceBySchedId = async (req, res) => {
-    const {id} = req.params
+    const { id } = req.params
     try {
         const data = await Model.find({}).populate({
             path: 'attendances',
             model: 'Attendance',
-            match: {schedule: id}
+            match: { schedule: id }
         })
-    
+
         if (!data) {
-            return res.status(404).json({error: 'No record found'})
+            return res.status(404).json({ error: 'No record found' })
         }
         return res.status(200).json(data)
     } catch (error) {
-        return res.status(404).json({error: error.message})
+        return res.status(404).json({ error: error.message })
     }
 }
 
@@ -123,13 +137,13 @@ const getUsersWithAttendance = async (req, res) => {
             path: 'attendances',
             model: 'Attendance',
         })
-    
+
         if (!data) {
-            return res.status(404).json({error: 'No record found'})
+            return res.status(404).json({ error: 'No record found' })
         }
         return res.status(200).json(data)
     } catch (error) {
-        return res.status(404).json({error: error.message})
+        return res.status(404).json({ error: error.message })
     }
 }
 
